f8676d41f04f64be859c0422b7a290955c4257a2
Matheus eli <matheuselisouzaferreira@hotmail.com>
2022-03-22 08:43:22 -0300

    accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
 
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index);
+    }
     const renderedItems = items.map((item, index) => {
         return (
             <React.Fragment key={index}>
-                <div className="title active" onClick={() => {console.log(`Title clicked! ${index}`)}}>
+                <div
+                    className="title active"
+                    onClick={() => onTitleClick(index)}>
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
@@ -14,10 +21,12 @@ const Accordion = ({ items }) => {
                 </div>
             </React.Fragment>
         );
-    })
+    });
+    
     return (
         <div className="ui styled accordion">
             {renderedItems}
+            <h1>{activeIndex}</h1>
         </div>
     );
 }
