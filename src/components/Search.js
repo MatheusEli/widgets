f0cd40207c707c30322af84b1ba8660ea10501bc
Matheus eli <matheuselisouzaferreira@hotmail.com>
2022-03-24 16:53:28 -0300

    dangerouslySetInnerHTML

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,48 @@
 import { useState, useEffect } from "react";
 import React from "react";
-import wikipedia from "../apis/wikipedia";
+import axios from "axios";
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
-        
-    });
-    
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    format: 'json',
+                    origin: '*',
+                    srsearch: term
+                }
+            });
+
+            setResults(data.query.search);
+        }
+
+        search();
+
+
+    }, [term]);
+
+
     const onSearchTyped = (event) => {
         setTerm(event.target.value);
     }
+
+    const renderedResults = results.map((result) => {
+        return (
+        <div className="item" key={result.pageid}>
+            <div className="content">
+                <div className="header">
+                    {result.title}
+                </div>
+                <span dangerouslySetInnerHTML={{ __html: result.snippet}}></span>
+            </div>
+        </div>
+        );
+    })
 
     return (
         <div>
@@ -20,6 +51,9 @@ const Search = () => {
                     <label>Enter Search theme</label>
                     <input value={term} onChange={onSearchTyped} className="input"></input>
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     );
