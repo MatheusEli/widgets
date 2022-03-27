968a8bc1d9f368ca2fe5cf4aeed38cc56eaedc3a
MatheusEli <matheuselisouzaferreira@hotmail.com>
2022-03-27 18:15:42 -0300

    debouncedTerm

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,60 +3,69 @@ import React from "react";
 import axios from "axios";
 
 const Search = () => {
-    const [term, setTerm] = useState('programming');
-    const [results, setResults] = useState([]);
+  const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+  const [results, setResults] = useState([]);
 
-    useEffect(() => {
-        const search = async () => {
-            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-                params: {
-                    action: 'query',
-                    list: 'search',
-                    format: 'json',
-                    origin: '*',
-                    srsearch: term
-                }
-            });
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 500);
 
-            setResults(data.query.search);
-        }
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
-        search();
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          format: "json",
+          origin: "*",
+          srsearch: debouncedTerm,
+        },
+      });
 
+      setResults(data.query.search);
+    };
 
-    }, [term]);
+    search();
 
+  }, [debouncedTerm]);
 
-    const onSearchTyped = (event) => {
-        setTerm(event.target.value);
-    }
+  const onSearchTyped = (event) => {
+    setTerm(event.target.value);
+  };
 
-    const renderedResults = results.map((result) => {
-        return (
-        <div className="item" key={result.pageid}>
-            <div className="content">
-                <div className="header">
-                    {result.title}
-                </div>
-                <span dangerouslySetInnerHTML={{ __html: result.snippet}}></span>
-            </div>
-        </div>
-        );
-    })
-
+  const renderedResults = results.map((result) => {
     return (
-        <div>
-            <div className="ui form">
-                <div className="field">
-                    <label>Enter Search theme</label>
-                    <input value={term} onChange={onSearchTyped} className="input"></input>
-                </div>
-            </div>
-            <div className="ui celled list">
-                {renderedResults}
-            </div>
+      <div className="item" key={result.pageid}>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
+      </div>
     );
-}
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search theme</label>
+          <input
+            value={term}
+            onChange={onSearchTyped}
+            className="input"
+          ></input>
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
+};
 
 export default Search;
